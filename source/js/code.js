f611cbdc48af9874b3b62cbe368cbc91df32e72a
Abo Anas <61202200+Ehab-Elshahat@users.noreply.github.com>
2020-03-10 01:51:31 +0300

    after ssh keygen

--- source/js/code.js
+++ source/js/code.js
@@ -1,7 +1,53 @@
 $(function () {
   'use strict';
-  $('.arrow').on('click', function () {
-    $(this).parent().parent('li').find('.sub-list').slideToggle()
+  // side bar menu sliding
+  $('a').on('click', function () {
+    $(this).parent('li').find('.sub-list').slideToggle();
+    $(this).children('.arrow').toggleClass('rotate');
   });
   
+  //moving sidebar and content section
+  $('.custome-2').on('click', function () {
+    $('.sidebar, .content').toggleClass('move')
+  });
+
+  // full screen browser
+  $('.full-screen').on('click', function () {
+    $(this).toggleClass('mini');
+    if ($(this).hasClass('mini')) {
+      openFullscreen() 
+    } else {
+      closeFullscreen()
+    }
+  }) 
 });
+
+
+/* Get the element you want displayed in fullscreen mode (a video in this example): */
+var elem = document.documentElement;
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
